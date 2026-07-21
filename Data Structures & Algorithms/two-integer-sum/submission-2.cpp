88f9fe345d0841095082e1427be704246a425c63
Divyansh Pratap Singh <130889408+divyansh-prks@users.noreply.github.com>
2026-07-21 19:16:48 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int ,int > mp ; 
+        // for (auto val : mp )
+        //isme indexing ka need pad rha to hum for loop lagyegne 
+
+        
+        for (int i = 0 ; i< nums.size() ;i++){
+            //does difference exist in map ? 
+
+            if (mp.find((target - nums[i])) == mp.end()){
+                mp[nums[i]] = i ;
+
+            }else {
+            return {nums[i] , j} ; 
+            }
+
+
+        }
+
+        
+    }
+};
