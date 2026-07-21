94348524f85bf1976e55a78f231bca054dd68f1b
Divyansh Pratap Singh <130889408+divyansh-prks@users.noreply.github.com>
2026-07-21 06:10:55 +0530

    Add: duplicate-integer - submission-14

---
+++ Data Structures & Algorithms/duplicate-integer/submission-14.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    bool hasDuplicate(vector<int>& nums) {
+        map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.find(nums[i]) != mp.end()) {  // 1  2 3 3
+                return true ;               // 1->1
+            }
+            mp[i] +=1;
+
+        }
+        return false;
+    }
+    
+};
