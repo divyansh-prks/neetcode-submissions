f59cbbb98ab81a013df45e48fc5746de86fde3b9
Divyansh Pratap Singh <130889408+divyansh-prks@users.noreply.github.com>
2026-07-21 17:47:53 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false ;
+        }
+
+        sort(s.begin() , s.end()) ;
+        sort(t.begin() , t.end()) ; 
+
+        first = 0 ; 
+        second = 0 ;
+    
+
+        while ( true){
+            if(s[first] == t[first]){
+                first ++ ; 
+                second ++ ; 
+            }else{
+                return false ; 
+            }
+        }
+
+        
+    return true ; 
+        
+    }
+};
+
+
+
