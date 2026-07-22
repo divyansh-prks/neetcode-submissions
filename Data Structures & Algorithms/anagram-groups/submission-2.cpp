04c958cfa369b86b740f8d3ca55de1d03978d0f0
Divyansh Pratap Singh <130889408+divyansh-prks@users.noreply.github.com>
2026-07-22 19:19:11 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<string , vector<string> > mp ; 
+        for (auto &el : strs){
+            string sorted = el ; 
+            sort(sorted.begin() , sorted.end()); 
+            mp[sorted] = el ; 
+        }
+
+        vector<vector<string>> final ; 
+        for (auto&el :mp) {
+            final.push_back(el.second); 
+        }
+    }
+};
