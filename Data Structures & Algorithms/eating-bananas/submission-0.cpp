b2e267b1c709c2d9240017ca6f583f13f23c2f54
Divyansh Pratap Singh <130889408+divyansh-prks@users.noreply.github.com>
2026-08-10 15:28:59 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+    }
+};
+
+      k  1 2 2 
+piles = [1,4,3,2], h = 9
+        1   2 3 4 
+
+   k 1   1  4  3 2 = 10 
+   k 2   1  2 2  1 = 6 hrs 
+
+
+        2  
+ decide your bananas-per-hour eating rate of k.
+each hour - can choose a pile of bana and eat k banana ()
+
+for (i -> K ){
+    k -> 1 
+        while(i < arr[i]){
+            subtract  = arr[i] -1 
+        }
+}
